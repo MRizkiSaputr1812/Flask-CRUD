b8847d60f0a3eb970ae13bb8633c0addaa636d84
MRizkiSaputr1812 <rizkiiisaputra18@gmail.com>
2025-06-30 15:01:25 +0700

    Delete Probabilitas.py directory

--- Probabilitas.py/Histogram.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-data = [1,2,2,3,3,3,4,4,4,4,5,5,6,7,8]
-
-plt.hist(data, bins=5, color='orange', edgecolor='black')
-plt.title("Contoh Histogram")
-plt.xlabel("Nilai")
-plt.ylabel("Frekuensi")
-plt.show()
--- Probabilitas.py/KonsepDasar.py
+++
@@ -1,8 +0,0 @@
-#print("Hello Word")
-
-#umur = 18
-#print("Aku Rizki umurku", umur, "tahun")
-
-#for i in range(5)
- #   print(i)
- 
